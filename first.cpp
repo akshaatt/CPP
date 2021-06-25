b99458fa00bf69d46413170f640c3332a2ae8749
hawkeyepirate <lordhawkeye25@gmail.com>
2021-06-25 17:53:44 +0530

    #

--- first.cpp
+++ first.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
-
+int a = 10;
+int b = 10;
+int c = 10;
+int d;
 int main(){
     cout << "Hemlo" << endl;
+    d= a+b+c;
+    cout << "Sum of a"<<a<<"and b"<<b<<"and c"<<c<<"is"<<d;
     return 0;
 }
